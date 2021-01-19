1ea79c78308deec5fe0861f919140efcc3f67aa4
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-19 21:55:00 +0200

    Basic Syntax

--- 01.Basic Syntax, Conditional Statements and Loops/05.numbersMtoN.js
+++ 01.Basic Syntax, Conditional Statements and Loops/05.numbersMtoN.js
@@ -1,6 +1,6 @@
-function solve(m,n) {
-    for(let i = m ; i >= n; i--) {
-        console.log(i);
-    }
+function solve(m, n) {
+  for (let i = m; i >= n; i--) {
+    console.log(i);
+  }
 }
-solve(6,2);
+solve(6, 2);
