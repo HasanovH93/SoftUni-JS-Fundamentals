788ec0369951bf373b7f1cdb5f3308e86d29b9b1
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-23 21:55:00 +0200

    Basic Syntax

--- 01.Basic Syntax, Conditional Statements and Loops/08.foreignLanguages.js
+++ 01.Basic Syntax, Conditional Statements and Loops/08.foreignLanguages.js
@@ -1,21 +1,11 @@
-
-
 function solve(input) {
+  if (input === "USA" || input === "England") {
+    console.log("English");
+  } else if (input === "Argentina" || input === "Mexico" || input === "Spain") {
+    console.log("Spanish");
+  } else {
+    console.log("unknown");
+  }
+}
 
-    if(input === "USA" || input === "England") {
-        console.log("English")
-    }else if(input === "Argentina" || input === "Mexico" || input === "Spain") {
-        console.log("Spanish")
-    }else {
-        console.log("unknown")
-    }
-    
-   
-    }
-
-solve("Spain")
-
-
-
-
-
+solve("Spain");
