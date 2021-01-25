f64cb068616984aa5daed75b3abbfd78c91b7e3c
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-25 21:55:00 +0200

    Basic Syntax

--- 01.Basic Syntax, Conditional Statements and Loops/10.divisibleBy3.js
+++ 01.Basic Syntax, Conditional Statements and Loops/10.divisibleBy3.js
@@ -1,19 +1,15 @@
 function solve() {
-    for (let i = 1; i <= 100; i++) {
-        
-        if(i  % 3 == 0) {
-            console.log(i)
-
-        }
-        
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+      console.log(i);
     }
-
+  }
 }
 
 // function solve2(){
 //     for (let i = 3; i <= 100; i+= 3) {
 //         console.log(i)
-        
+
 //     }
 // }
 // solve2()
