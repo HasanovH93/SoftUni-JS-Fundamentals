adc014915c7eece790ef1b62c150406f3f7f645e
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-25 13:25:33 +0200

    Basic Syntax

--- 01.Basic Syntax, Conditional Statements and Loops/11.sumOfOddNumbers.js
+++ 01.Basic Syntax, Conditional Statements and Loops/11.sumOfOddNumbers.js
@@ -1,15 +1,11 @@
 function solve(count) {
-    let numForPrint = 1;
-    let sum = 0;
-    for (let i = 1; i <= count; i++) {
-        console.log(numForPrint);
-        sum += numForPrint;
-        numForPrint += 2;
-
-        
-        
-        
-    }
-    console.log(`Sum: ${sum}`)
+  let numForPrint = 1;
+  let sum = 0;
+  for (let i = 1; i <= count; i++) {
+    console.log(numForPrint);
+    sum += numForPrint;
+    numForPrint += 2;
+  }
+  console.log(`Sum: ${sum}`);
 }
-solve(15)
+solve(15);
--- 01.Basic Syntax, Conditional Statements and Loops/petShop.js
+++ 01.Basic Syntax, Conditional Statements and Loops/petShop.js
@@ -1,16 +1,11 @@
-function petshop (input){
+function petshop(input) {
+  let dogs = Number(input[0]);
 
-    let dogs = Number(input[0]);
-    
-    let animals = Number(input[1]);
-    
-    let total = (dogs * 2.50 + animals * 4)
-    
-     
-    
-    console.log(total.toFixed(2) + "lv.");
-    
-    }
+  let animals = Number(input[1]);
 
+  let total = dogs * 2.5 + animals * 4;
 
-petshop([13,9])
+  console.log(total.toFixed(2) + "lv.");
+}
+
+petshop([13, 9]);
