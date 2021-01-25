33d5996d7133d20c89f83fb3fba4fd237fdce2df
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-25 21:55:00 +0200

    Basic Syntax

--- 01.Basic Syntax, Conditional Statements and Loops/09.theatre.js
+++ 01.Basic Syntax, Conditional Statements and Loops/09.theatre.js
@@ -1,41 +1,31 @@
 function solve(day, age) {
-    if (age >= 0 && age <= 18) {
-        if (day == "Weekday") {
-            console.log("12$")
-        } else if (day == "Weekend") {
-            console.log("15$")
-        } else if (day == "Holiday") {
-            console.log("5$")
-        }
+  if (age >= 0 && age <= 18) {
+    if (day == "Weekday") {
+      console.log("12$");
+    } else if (day == "Weekend") {
+      console.log("15$");
+    } else if (day == "Holiday") {
+      console.log("5$");
     }
-    else if (age >= 18 && age <= 64) {
-        if (day == "Weekday") {
-            console.log("18$")
-        } else if (day == "Weekend") {
-            console.log("20$")
-        } else if (day == "Holiday") {
-            console.log("12$")
-        }
+  } else if (age >= 18 && age <= 64) {
+    if (day == "Weekday") {
+      console.log("18$");
+    } else if (day == "Weekend") {
+      console.log("20$");
+    } else if (day == "Holiday") {
+      console.log("12$");
     }
-    else if (age >= 64 && age <= 122) {
-        if (day == "Weekday") {
-            console.log("12$")
-        } else if (day == "Weekend") {
-            console.log("15$")
-        } else if (day == "Holiday") {
-            console.log("10$")
-        }
-    } 
-    else  {
-        console.log("Error!");
+  } else if (age >= 64 && age <= 122) {
+    if (day == "Weekday") {
+      console.log("12$");
+    } else if (day == "Weekend") {
+      console.log("15$");
+    } else if (day == "Holiday") {
+      console.log("10$");
     }
+  } else {
+    console.log("Error!");
+  }
 }
 
-
-        
-    
-
-
-solve("Weekday", 42),
-    solve("Holiday", -12),
-    solve("Holiday", 15)
+solve("Weekday", 42), solve("Holiday", -12), solve("Holiday", 15);
