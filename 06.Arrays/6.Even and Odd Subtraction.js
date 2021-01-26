6244be3ec32ac6e3cab65ded0aff16081f200c8f
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-26 15:14:50 +0200

    Arrays

--- 06.Arrays/6.Even and Odd Subtraction.js
+++ 06.Arrays/6.Even and Odd Subtraction.js
@@ -1,21 +1,17 @@
 function solve(arr) {
-
-    let sumOdd = 0;
-    let sumEven = 0;
-
-    for(let unparsed of arr){
-        let number = Number(unparsed)
-        if(number % 2 === 0) {
-            sumEven += number;
-        }else {
-            sumOdd += number;
-        }
-       
-
+  let sumEven = 0;
+  let sumOdd = 0;
+  for (const number of arr) {
+    if (number % 2 === 0) {
+      sumEven += Number(number);
     }
-     let diff = sumEven - sumOdd;
-    console.log(diff)
-
+    else {
+        sumOdd += Number(number);
+    }
+    
+  }
+  
+  console.log(sumEven - sumOdd);
 }
 
-solve([1,2,3,4,5,6])
+solve([1,2,3,4,5,6]);
