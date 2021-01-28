63a6f0b6e29f68a7796f7e0e084e50241880143f
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-28 15:14:50 +0200

    Arrays

--- 06.Arrays/2.daysOfWeeks.js
+++ 06.Arrays/2.daysOfWeeks.js
@@ -1,6 +1,21 @@
 function solve(num) {
 
+    let days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Fiday",
+        "Satuday",
+        "Sunday"
+    ];
+
+    if(num >= 1 && num <= 7){
+        console.log(days[num - 1]);
+    }else {
+        console.log("Invalid day!")
+    }
 
 
 }
-solve(3)
+solve(7)
