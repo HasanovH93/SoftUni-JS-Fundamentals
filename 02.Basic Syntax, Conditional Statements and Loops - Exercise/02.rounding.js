c5800fe8be5a70ed65a437c447fd7a0a184b0225
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-28 13:25:33 +0200

    Basic Syntax Exercise

--- 02.Basic Syntax, Conditional Statements and Loops - Exercise/02.rounding.js
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/02.rounding.js
@@ -1,13 +1,12 @@
-function solve(num,prec) {
+function solve(num, prec) {
+  let ourNum = num;
+  let precision = prec;
 
-    let ourNum = num;
-    let precision = prec;
-
-    if (precision > 15) {
-        precision = 15;
-    }
-    let result = ourNum.toFixed(precision);
-    console.log(parseFloat(result));
+  if (precision > 15) {
+    precision = 15;
+  }
+  let result = ourNum.toFixed(precision);
+  console.log(parseFloat(result));
 }
 
-solve(3.1501515123123343943430,2)
+solve(3.150151512312334394343, 2);
