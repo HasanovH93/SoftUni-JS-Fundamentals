1f5ccf7edf870862d6580ba77d96ed63b2f853f9
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-28 15:14:50 +0200

    Arrays

--- 06.Arrays/1.Sum First and Last Array Elements.js
+++
@@ -1,5 +0,0 @@
-function solve(arr) {
-  
-}
-
-solve([20,30,40])
---
+++ 06.Arrays/1.sumFirstAndLastElement.js
@@ -0,0 +1,6 @@
+function solve(arr) {
+    let sum = arr[0] + arr[arr.length - 1];
+    console.log(sum)
+  }
+  
+  solve([20,30,40])
