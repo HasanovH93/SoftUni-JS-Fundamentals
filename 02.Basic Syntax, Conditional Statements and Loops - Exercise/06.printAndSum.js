4a811c7cf805707535dc3a606bece03fbdcd567d
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-29 13:25:33 +0200

    Basic Syntax Exercise

--- 02.Basic Syntax, Conditional Statements and Loops - Exercise/06.printAndSum.js
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/06.printAndSum.js
@@ -1,12 +1,11 @@
-function solve(num1,num2){
-    let sum = 0;
-    let buff = "";
-    for(let i = num1 ; i<= num2; i++){
-        buff += `${i} `;
-        sum += i;
-    }
+function solve(num1, num2) {
+  let sum = 0;
+  let buff = "";
+  for (let i = num1; i <= num2; i++) {
+    buff += `${i} `;
+    sum += i;
+  }
   console.log(buff.trim());
   console.log(`Sum: ${sum}`);
-  
 }
-solve(5,10);
+solve(5, 10);
