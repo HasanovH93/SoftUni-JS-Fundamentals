c6b567a663bbad606fde71920985f5ffb1f2deb1
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-29 13:25:33 +0200

    Basic Syntax Exercise

--- 02.Basic Syntax, Conditional Statements and Loops - Exercise/05.leapYear.js
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/05.leapYear.js
@@ -1,11 +1,10 @@
 function solve(input) {
-    let year = input;
-    if ((year % 4 == 0 && year % 100 !== 0) || year % 400 === 0) {
-        console.log("yes");
-    }else {
-        console.log("no")
-    }
-    
+  let year = input;
+  if ((year % 4 == 0 && year % 100 !== 0) || year % 400 === 0) {
+    console.log("yes");
+  } else {
+    console.log("no");
+  }
 }
 
-solve(1984)
+solve(1984);
