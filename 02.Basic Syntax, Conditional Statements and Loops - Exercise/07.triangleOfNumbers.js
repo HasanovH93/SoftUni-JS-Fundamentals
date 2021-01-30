f260fc36f98503e5f6beff70630a78179cdee529
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-30 13:25:33 +0200

    Basic Syntax Exercise

--- 02.Basic Syntax, Conditional Statements and Loops - Exercise/07.triangleOfNumbers.js
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/07.triangleOfNumbers.js
@@ -1,9 +1,7 @@
 function solve(num) {
-    for (let i = 1; i <= num; i++) {
-      console.log(`${i} `.repeat(i))
-      
+  for (let i = 1; i <= num; i++) {
+    console.log(`${i} `.repeat(i));
   }
 }
-
 
 solve(5);
