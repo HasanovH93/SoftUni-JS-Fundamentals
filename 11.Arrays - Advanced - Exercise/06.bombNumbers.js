fe1c4865e52c365b3eb2617d1eb53caac4f31709
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-31 15:23:00 +0200

    Advanced Arrays

--- 11.Arrays - Advanced - Exercise/06.bombNumbers.js
+++ 11.Arrays - Advanced - Exercise/06.bombNumbers.js
@@ -1,24 +1,6 @@
 function bombDetonate(sequence, bomb) {
 
-    let specialNumber = bomb[0];
-    let bombPower = bomb[1];
-
-    while (sequence.includes(specialNumber)) {
-        let index = sequence.indexOf(specialNumber);
-        let elementsToRemove = bombPower * 2 + 1;
-        let startIndex = index - bombPower;
-
-        if (startIndex < 0) {
-            elementsToRemove += startIndex;
-            startIndex = 0;
-
-        }
-        sequence.splice(startIndex, elementsToRemove);
-    }
-    let finalResult = sequence.reduce((a, b) => {
-        return a + b
-    },0);
-    console.log(finalResult);
+  
 
 }
 
--- 11.Arrays - Advanced - Exercise/07.searchForNumber.js
+++ 11.Arrays - Advanced - Exercise/07.searchForNumber.js
@@ -1,24 +1,21 @@
 function search(collection,numbers) {
     
     let elementsToCut = numbers[0];
-    let elementsToDel = numbers[1];
-    let elementToFind = numbers[2];
+    let elementsToDelete = numbers[1];
+    let elementsToFind = numbers[2];
 
-
-    let modifiedArr = collection.slice(0,elementsToCut);
-        modifiedArr.splice(0,elementsToDel);
-        let counter = 0;
-        let modifiedArrL = modifiedArr.length;
-
-        for(let i = 0; i < modifiedArrL; i++ ) {
-            if(elementToFind === modifiedArr[i]) {
-                counter++
-            }
-            
-        }
+    let modifiedArr = collection.slice(0,elementsToCut)
+    modifiedArr.splice(0,elementsToDelete);
     
+    let counter = 0;
+    for(let i = 0; i < modifiedArr.length; i++) {
+       if(elementsToFind === modifiedArr[i]){
+           counter++;
+       }
 
-    console.log(`Number ${elementToFind} occurs ${counter} times.`);
+    }
+
+    console.log(`Number ${elementsToFind} occurs ${counter} times.`);
 
     
 
