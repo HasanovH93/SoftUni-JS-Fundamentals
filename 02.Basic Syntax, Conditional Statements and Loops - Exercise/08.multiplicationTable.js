5b871ba2899b4e42a908d6b0b8a6ac65b372eccc
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-01 13:25:33 +0200

    Basic Syntax Exercise

--- 02.Basic Syntax, Conditional Statements and Loops - Exercise/08.multiplicationTable.js
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/08.multiplicationTable.js
@@ -1,13 +1,8 @@
 function solve(num) {
-
-    let number = num;
-    for(let i = 1; i <= 10; i++){
-        console.log(`${number} X ${i} = ${number * i}`)
-    
-
-    }
-   
-
+  let number = num;
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${number} X ${i} = ${number * i}`);
+  }
 }
 
-solve(5)
+solve(5);
