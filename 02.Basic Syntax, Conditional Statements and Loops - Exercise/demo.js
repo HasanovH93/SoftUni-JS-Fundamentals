664ad1d8bd273c2ef285e0a95213f8d7696e658b
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-05 13:25:33 +0200

    Basic Syntax Exercise

--- 02.Basic Syntax, Conditional Statements and Loops - Exercise/demo.js
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/demo.js
@@ -1,33 +1,24 @@
 function solve(input) {
-    
-    let index = 0;
-    let username = input[index++];
-    
-    let password = username.split('').reverse().join('');
-    let tryPass = input[index++];
-    let counter = 1;
-    let isLogged = true;
-    while(tryPass !== password) {
-        
-      counter++;
-      console.log(`Incorrect password. Try again.`)
-      if(counter === 4){
-          
-          isLogged = false;
-          console.log(`User ${username} blocked!`)
-          break;
-      }
+  let index = 0;
+  let username = input[index++];
 
-      tryPass = input[index++];
-    
+  let password = username.split("").reverse().join("");
+  let tryPass = input[index++];
+  let counter = 1;
+  let isLogged = true;
+  while (tryPass !== password) {
+    counter++;
+    console.log(`Incorrect password. Try again.`);
+    if (counter === 4) {
+      isLogged = false;
+      console.log(`User ${username} blocked!`);
+      break;
     }
-   if(isLogged) {
-    console.log(`User ${username} logged in.`)
-   }
-    
-    
 
-
- 
+    tryPass = input[index++];
+  }
+  if (isLogged) {
+    console.log(`User ${username} logged in.`);
+  }
 }
-solve(['sunny','rainy','cloudy','sunny','not sunny']);
+solve(["sunny", "rainy", "cloudy", "sunny", "not sunny"]);
