41a648feb23a1c83eb38d39f6c2db67e2285fd33
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-06 13:25:33 +0200

    Basic Syntax Exercise

--- 02.Basic Syntax, Conditional Statements and Loops - Exercise/nextDay.js
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/nextDay.js
@@ -1,15 +1,10 @@
-function nextDay(year,month,day) { 
+function nextDay(year, month, day) {
+  let nextDay = new Date(year, month - 1, day + 1);
+  let newYear = nextDay.getFullYear();
+  let newMonth = nextDay.getMonth() + 1;
+  let newDate = nextDay.getDate();
 
-    let nextDay = new Date(year, month - 1, day +1 );
-    let newYear = nextDay.getFullYear();
-    let newMonth = nextDay.getMonth() + 1;
-    let newDate = nextDay.getDate();
-
-    console.log(`${newYear}-${newMonth}-${newDate}`)
-
-
-
-
+  console.log(`${newYear}-${newMonth}-${newDate}`);
 }
 
-nextDay(2016, 9, 30)
+nextDay(2016, 9, 30);
