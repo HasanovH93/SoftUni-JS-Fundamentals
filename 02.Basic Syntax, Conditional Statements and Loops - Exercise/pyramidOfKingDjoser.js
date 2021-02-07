c2888d670f770ff8d592eb1976809a8de2b003df
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-07 13:25:33 +0200

    Basic Syntax Exercise

--- 02.Basic Syntax, Conditional Statements and Loops - Exercise/pyramidOfKingDjoser.js
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/pyramidOfKingDjoser.js
@@ -1,49 +1,50 @@
-function pyramidOfKingDjoser(base , step){
-    let totalStone = 0;
-    let totalMarble = 0;
-    let totalLapis = 0;
+function pyramidOfKingDjoser(base, step) {
+  let totalStone = 0;
+  let totalMarble = 0;
+  let totalLapis = 0;
 
-    let row = 0;
-    row ++;
-    let currentSize = base;
+  let row = 0;
+  row++;
+  let currentSize = base;
 
-    while(currentSize > 2){
-        let marble = (currentSize * 4) - 4;
-        let stone = (currentSize * currentSize) - marble;
+  while (currentSize > 2) {
+    let marble = currentSize * 4 - 4;
+    let stone = currentSize * currentSize - marble;
 
-        if(row % 5 === 0){
-            totalLapis += marble;
-        } else {
-            totalMarble += marble;
-        }
-
-        totalStone += stone;
-        currentSize -= 2;
-        row++;
-    }
-    
-    let totalGold = 0;
-
-    if(currentSize <= 1){
-        totalGold = 1;
+    if (row % 5 === 0) {
+      totalLapis += marble;
     } else {
-        totalGold = currentSize * 2;
+      totalMarble += marble;
     }
-    
-    totalStone = Math.ceil(totalStone * step);
-    totalMarble = Math.ceil(totalMarble * step);
-    totalLapis = Math.ceil(totalLapis * step);
-    totalGold = Math.ceil(totalGold * step);
 
-    let height = Math.floor(row * step);
+    totalStone += stone;
+    currentSize -= 2;
+    row++;
+  }
 
-    let print = `Stone required: ${totalStone}\n` +
+  let totalGold = 0;
+
+  if (currentSize <= 1) {
+    totalGold = 1;
+  } else {
+    totalGold = currentSize * 2;
+  }
+
+  totalStone = Math.ceil(totalStone * step);
+  totalMarble = Math.ceil(totalMarble * step);
+  totalLapis = Math.ceil(totalLapis * step);
+  totalGold = Math.ceil(totalGold * step);
+
+  let height = Math.floor(row * step);
+
+  let print =
+    `Stone required: ${totalStone}\n` +
     `Marble required: ${totalMarble}\n` +
     `Lapis Lazuli required: ${totalLapis}\n` +
     `Gold required: ${totalGold}\n` +
     `Final pyramid height: ${height}`;
 
-    console.log(print);
+  console.log(print);
 }
 
-pyramidOfKingDjoser(23,0.5);
+pyramidOfKingDjoser(23, 0.5);
