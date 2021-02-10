9855a9247ac146d773891f463db819b727eda0ec
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-10 21:00:00 +0200

    Exam Exercise

--- 04.Data Type and Variables - Exercise/9. Gladiator Expenses.js
+++ 04.Data Type and Variables - Exercise/9. Gladiator Expenses.js
@@ -1,24 +1,20 @@
-function solve(lostFights,helmetPrice,swordPrice,shieldPrice,armorPrice) {
+function solve(lostFights, helmetPrice, swordPrice, shieldPrice, armorPrice) {
+  let price = 0;
 
-
-    let price = 0;
-    for(let i = 1;i<= lostFights;i++){
-         if(i % 2 === 0) {
-             price += helmetPrice;
-         } 
-          if (i % 3 === 0){
-             price +=swordPrice;
-         }
-          if (i % 6 === 0){
-            price +=shieldPrice;
-         }
-          if (i % 12 === 0) {
-             price += armorPrice;
-         }
-         
-         
+  for (let i = 1; i <= lostFights; i++) {
+    if (i % 2 === 0) {
+      price += helmetPrice;
+    }
+    if (i % 3 === 0) {
+      price += swordPrice;
+    }
+    if (i % 6 === 0) {
+      price += shieldPrice;
+    }
+    if (i % 12 === 0) {
+      price += armorPrice;
+    }
+  }
+  console.log(`Gladiator expenses: ${price.toFixed(2)} aureus`);
 }
-
-console.log(`Gladiator expenses: ${price.toFixed(2)} aureus`);
-}
-solve(7,2,3,4,5)
+solve(7, 2, 3, 4, 5);
