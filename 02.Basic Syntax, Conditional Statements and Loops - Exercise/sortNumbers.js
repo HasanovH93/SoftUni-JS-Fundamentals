d75a7d11c39998cdca0098335eaf30a2bf5ae920
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-10 13:25:33 +0200

    Basic Syntax Exercise

--- 02.Basic Syntax, Conditional Statements and Loops - Exercise/sortNumbers.js
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/sortNumbers.js
@@ -1,39 +1,32 @@
-function sortNumbers(fNum,sNum,tNum){
-
-
-      if(fNum > sNum && fNum > tNum){
-          console.log(fNum);
-          if(sNum > tNum){
-            console.log(sNum);
-            console.log(tNum);
-        }else {
-            console.log(tNum);
-            console.log(sNum);
-        }
-      }else if(sNum > fNum && sNum > tNum){
-                 
-        console.log(sNum);
-        if(fNum > tNum){
-            console.log(fNum);
-            console.log(tNum);
-        }else {
-            console.log(tNum);
-            console.log(fNum);
-        }
-      } else if(tNum > fNum && tNum > sNum){
-          console.log(tNum)
-          if(fNum > sNum){
-              console.log(fNum);
-              console.log(sNum)
-          }else {
-              console.log(sNum);
-              console.log(fNum)
-          }
-      }
-      
-   
-
-
+function sortNumbers(fNum, sNum, tNum) {
+  if (fNum > sNum && fNum > tNum) {
+    console.log(fNum);
+    if (sNum > tNum) {
+      console.log(sNum);
+      console.log(tNum);
+    } else {
+      console.log(tNum);
+      console.log(sNum);
+    }
+  } else if (sNum > fNum && sNum > tNum) {
+    console.log(sNum);
+    if (fNum > tNum) {
+      console.log(fNum);
+      console.log(tNum);
+    } else {
+      console.log(tNum);
+      console.log(fNum);
+    }
+  } else if (tNum > fNum && tNum > sNum) {
+    console.log(tNum);
+    if (fNum > sNum) {
+      console.log(fNum);
+      console.log(sNum);
+    } else {
+      console.log(sNum);
+      console.log(fNum);
+    }
+  }
 }
 
-sortNumbers(3,4,5)
+sortNumbers(3, 4, 5);
