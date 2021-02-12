1714a9c636e97221d815cb2215771fd83bbdc66f
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-12 20:21:55 +0200

    Data Type and Variables

--- 03.Data Type and Variables/2.rightPlace.js
+++ 03.Data Type and Variables/2.rightPlace.js
@@ -1,10 +1,9 @@
-function solve(firstString,char,secondString){
-   let newString = firstString.replace("_", char);
+function solve(firstString, char, secondString) {
+  let newString = firstString.replace("_", char);
 
-   let result = (newString === secondString) ? 
-   "Matched" : "Not Matched"
-   console.log(result);
+  let result = newString === secondString ? "Matched" : "Not Matched";
+  console.log(result);
 }
 
-solve("Str_ng","I","Strong")
-solve("Str_ng","i","String")
+solve("Str_ng", "I", "Strong");
+solve("Str_ng", "i", "String");
