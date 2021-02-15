ec0e5b350a96fcdfa853239d7f37c1900852b5a8
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-15 20:21:55 +0200

    Data Type and Variables

--- 03.Data Type and Variables/04.amazingNumbers.js
+++ 03.Data Type and Variables/04.amazingNumbers.js
@@ -6,12 +6,8 @@ function solve(num) {
     sum += Number(str[i]);
   }
   let isContaining9 = sum.toString().includes("9");
-  let result = isContaining9 ? 
-  `${sum} Amazing? true` : 
-  `${num} Amazing? false`;
+  let result = isContaining9 ? `${sum} Amazing? true` : `${num} Amazing? false`;
   console.log(result);
-
 }
-  
 
 solve(1233);
