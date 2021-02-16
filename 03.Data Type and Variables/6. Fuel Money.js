79eb5226aae044f345a65dccb077617dac5a4a32
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-16 20:21:55 +0200

    Data Type and Variables

--- 03.Data Type and Variables/6. Fuel Money.js
+++ 03.Data Type and Variables/6. Fuel Money.js
@@ -1,8 +1,7 @@
-function solve(distance,passengers,price) {
-    
-    let fuel = distance / 100 * 7;
-    fuel += passengers * 0.1;
-    let costs = fuel * price 
-    console.log(`Needed money for trip is ${costs}lv.`);
+function solve(distance, passengers, price) {
+  let fuel = (distance / 100) * 7;
+  fuel += passengers * 0.1;
+  let costs = fuel * price;
+  console.log(`Needed money for trip is ${costs}lv.`);
 }
-solve(260, 9, 2.49)
+solve(260, 9, 2.49);
