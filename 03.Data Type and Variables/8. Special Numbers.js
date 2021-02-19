8ec2ce9263906a7561a793138a8a72cc7466ac64
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-19 20:21:55 +0200

    Data Type and Variables

--- 03.Data Type and Variables/8. Special Numbers.js
+++ 03.Data Type and Variables/8. Special Numbers.js
@@ -1,18 +1,18 @@
 function solve(num) {
-  for(let i = 1; i <= num; i++) {
-      let sum = 0;
-      let tempNum = i;
-      while(tempNum > 0) {
-        sum += tempNum % 10;
-        tempNum = Math.floor(tempNum / 10);
-      }
-      let isSpecial
-      if(sum === 5 || sum === 7 || sum === 11){
-          isSpecial = "True"
-      }else {
-         isSpecial = "False"
-      }
-      console.log(`${i} -> ${isSpecial} `)
+  for (let i = 1; i <= num; i++) {
+    let sum = 0;
+    let tempNum = i;
+    while (tempNum > 0) {
+      sum += tempNum % 10;
+      tempNum = Math.floor(tempNum / 10);
+    }
+    let isSpecial;
+    if (sum === 5 || sum === 7 || sum === 11) {
+      isSpecial = "True";
+    } else {
+      isSpecial = "False";
+    }
+    console.log(`${i} -> ${isSpecial} `);
   }
 }
-solve(10)
+solve(10);
