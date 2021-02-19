2634340664bcd5857ef306d55211b46416cd81ae
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-19 20:21:55 +0200

    Data Type and Variables Exercise

--- 04.Data Type and Variables - Exercise/1. Sum Digits.js
+++ 04.Data Type and Variables - Exercise/1. Sum Digits.js
@@ -1,14 +1,10 @@
 function solve(input) {
   let str = input.toString();
-  let sum =  0;
-  for(let i = 0; i< str.length; i++) {
-     
-    sum += Number(str[i])
-  
-      
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    sum += Number(str[i]);
   }
-  console.log(sum)
-
+  console.log(sum);
 }
 
-solve(245678)
+solve(245678);
