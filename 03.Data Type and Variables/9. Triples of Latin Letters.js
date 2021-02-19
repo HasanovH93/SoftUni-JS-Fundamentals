92afb9b1dd624d1d65ac94b3a413e15fb02635b3
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-19 20:21:55 +0200

    Data Type and Variables

--- 03.Data Type and Variables/9. Triples of Latin Letters.js
+++ 03.Data Type and Variables/9. Triples of Latin Letters.js
@@ -1,18 +1,15 @@
 function solve(num) {
-
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) {
       for (let k = 0; k < num; k++) {
+        let char1 = String.fromCharCode(97 + i);
+        let char2 = String.fromCharCode(97 + j);
+        let char3 = String.fromCharCode(97 + k);
 
-      let char1 = String.fromCharCode(97 + i)
-      let char2 = String.fromCharCode(97 + j)
-      let char3 = String.fromCharCode(97 + k)
-          
-      console.log(char1 + char2 + char3)
+        console.log(char1 + char2 + char3);
       }
     }
   }
-
 }
 
 solve(3);
