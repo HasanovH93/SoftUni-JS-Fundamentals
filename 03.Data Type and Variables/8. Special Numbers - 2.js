8f6bfd77ad7e12267081981404f8701d8bff77c2
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-19 20:21:55 +0200

    Data Type and Variables

--- 03.Data Type and Variables/8. Special Numbers - 2.js
+++ 03.Data Type and Variables/8. Special Numbers - 2.js
@@ -1,20 +1,18 @@
 function solve(num) {
-    for(let i = 1; i <= num; i++){
-        let sum = 0;
-        let stringNum = i.toString();
-        for( j= 0; j <stringNum.length; j++) {
-            sum += Number(stringNum[j]);
-        }
-        let result 
-        if(sum == 5 || sum == 7 || sum == 11) {
-            result = "True"
-
-        }else {
-            result = "False"
-        }
-
-
-        console.log(`${i} -> ${result}`)
+  for (let i = 1; i <= num; i++) {
+    let sum = 0;
+    let stringNum = i.toString();
+    for (j = 0; j < stringNum.length; j++) {
+      sum += Number(stringNum[j]);
     }
+    let result;
+    if (sum == 5 || sum == 7 || sum == 11) {
+      result = "True";
+    } else {
+      result = "False";
+    }
+
+    console.log(`${i} -> ${result}`);
+  }
 }
-solve(15)
+solve(15);
