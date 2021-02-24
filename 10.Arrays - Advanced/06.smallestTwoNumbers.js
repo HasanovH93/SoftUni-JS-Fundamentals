94281046549cb311281a5be8d697ad860d30f895
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-24 21:55:00 +0200

    Advanced Arrays

--- 10.Arrays - Advanced/06.smallestTwoNumbers.js
+++ 10.Arrays - Advanced/06.smallestTwoNumbers.js
@@ -1,7 +1,7 @@
 function smallestNumbers(arr){
 
     let sorted = arr.sort((a,b) => a-b).slice(0,2).join(' ');
-    console.log(sorted)
+    console.log(sorted);
 
 }
-smallestNumbers([30, 15, 50, 5])
+smallestNumbers([30, 15, 50, 5]);
--- 10.Arrays - Advanced/07.listOfProducts.js
+++ 10.Arrays - Advanced/07.listOfProducts.js
@@ -1,11 +1,14 @@
-function productsList(input) {
-  let sortedInput = input.sort((a, b) => {
-    return a.localeCompare(b);
-  });
-  let arrL = sortedInput.length;
-  for (let i = 0; i < arrL; i++) {
-    console.log(`${i + 1}.${sortedInput[i]} `);
-  }
+function productsList(arr) {
+
+    let sortedArr = arr.sort((a,b)=>a.localeCompare(b));
+    let sortedL = sortedArr.length;
+   
+    for(let i = 0; i<sortedL;i++){
+        console.log(`${i+1}.${sortedArr[i]}`);
+    }
+
+
+
 }
 
 productsList(["Potatoes", "Tomatoes", "Onions", "Apples"]);
