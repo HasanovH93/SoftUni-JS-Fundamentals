332bd85fc60d2412810fd960a68c34111228618b
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-25 15:23:00 +0200

    Advanced Arrays

--- 10.Arrays - Advanced/07.listOfProducts.js
+++ 10.Arrays - Advanced/07.listOfProducts.js
@@ -1,14 +1,10 @@
 function productsList(arr) {
+  let sortedArr = arr.sort();
+  let sortedL = sortedArr.length;
 
-    let sortedArr = arr.sort();
-    let sortedL = sortedArr.length;
-   
-    for(let i = 0; i<sortedL;i++){
-        console.log(`${i+1}.${sortedArr[i]}`);
-    }
-
-
-
+  for (let i = 0; i < sortedL; i++) {
+    console.log(`${i + 1}.${sortedArr[i]}`);
+  }
 }
 
 productsList(["Potatoes", "Tomatoes", "Onions", "Apples"]);
--- 11.Arrays - Advanced - Exercise/02.distrinctAray.js
+++ 11.Arrays - Advanced - Exercise/02.distrinctAray.js
@@ -1,14 +1,11 @@
-function district(input) {
-  let result = [];
-
-  for (let num of input) {
-    if (!result.includes(num)) {
-      result.push(num);
+function district(arr) {
+  let resultArr = [];
+  for (const element of arr) {
+    if (!resultArr.includes(element)) {
+      resultArr.push(element);
     }
   }
-
-  console.log(result.join(" "));
-
+  console.log(resultArr.join(' '));
 }
 
 district([7, 8, 9, 7, 2, 3, 4, 1, 2]);
