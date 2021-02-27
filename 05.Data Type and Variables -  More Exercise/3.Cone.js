6fb17be9c6cd11d161d21a978f8f77231e5a3d93
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-27 20:21:55 +0200

    Data Type and Variables Exercise

--- 05.Data Type and Variables -  More Exercise/3.Cone.js
+++ 05.Data Type and Variables -  More Exercise/3.Cone.js
@@ -1,11 +1,11 @@
-    function cone(radius,height) {
-        let r = Number(radius)
-        let h = Number(height)
-        let s = Math.sqrt(r * r + h * h);
-        let volume = Math.PI * r * r * h / 3;
-        console.log("volume = " + volume.toFixed(4));
-        let area = Math.PI * r * (r + s);
-        console.log("area = " + area.toFixed(4));
-    }
+function cone(radius, height) {
+  let r = Number(radius);
+  let h = Number(height);
+  let s = Math.sqrt(r * r + h * h);
+  let volume = (Math.PI * r * r * h) / 3;
+  console.log("volume = " + volume.toFixed(4));
+  let area = Math.PI * r * (r + s);
+  console.log("area = " + area.toFixed(4));
+}
 
-cone(5,7)
+cone(5, 7);
