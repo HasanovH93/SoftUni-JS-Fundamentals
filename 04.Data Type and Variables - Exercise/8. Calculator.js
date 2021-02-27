c5abdb1a694a21d408acb30538e66c3f1df9c8f1
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-27 20:21:55 +0200

    Data Type and Variables Exercise

--- 04.Data Type and Variables - Exercise/8. Calculator.js
+++ 04.Data Type and Variables - Exercise/8. Calculator.js
@@ -1,17 +1,23 @@
 function solve(fNum, oper, sNum) {
-    let firstNumber = fNum;
-    let operator = oper;
-    let secondNumber = sNum;
-    let result = 0;
+  let firstNumber = fNum;
+  let operator = oper;
+  let secondNumber = sNum;
+  let result = 0;
 
-    switch (oper) {
-        case '+': result = firstNumber + secondNumber; break;
-        case '-': result = firstNumber - secondNumber; break;
-        case '*': result = firstNumber * secondNumber; break;
-        case '/': result = firstNumber / secondNumber; break;
-
-    }
-    console.log(result.toFixed(2));
-
+  switch (oper) {
+    case "+":
+      result = firstNumber + secondNumber;
+      break;
+    case "-":
+      result = firstNumber - secondNumber;
+      break;
+    case "*":
+      result = firstNumber * secondNumber;
+      break;
+    case "/":
+      result = firstNumber / secondNumber;
+      break;
+  }
+  console.log(result.toFixed(2));
 }
-solve(5, "+", 10)
+solve(5, "+", 10);
