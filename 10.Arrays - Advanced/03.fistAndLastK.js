e18577089fca991ca6f1a1582dfca9f4ca312fe0
Hasan Hasanov <hasanovh14@gmail.com>
2021-02-28 21:55:00 +0200

    Advanced Arrays Exercise

---
+++ 10.Arrays - Advanced/03.fistAndLastK.js
@@ -0,0 +1,12 @@
+function firstAndLast(arr) {
+   let k = arr.shift();
+   let newArr = [];
+   let firstK =  arr.slice(0,k);
+   let lastK = arr.slice(-k)
+  
+   console.log(firstK.join(' '));
+   console.log(lastK.join(' '));
+
+}
+
+firstAndLast([2, 7, 8, 9]);
