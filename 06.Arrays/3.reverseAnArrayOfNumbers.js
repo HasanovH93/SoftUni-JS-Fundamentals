3901bd8ac9a5b181ca8e8bb7179ec5555e43bf1c
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-02 21:55:00 +0200

    Arrays

--- 06.Arrays/3.reverseAnArrayOfNumbers.js
+++ 06.Arrays/3.reverseAnArrayOfNumbers.js
@@ -1,17 +1,11 @@
 function reverse(n, arr) {
-   
-    let newArr = [];
-    for(let i = 0; i< n; i++){
-        newArr.push(arr[i]);
-        
-    }
-    let output = "";
-    for(let i = newArr.length -1 ; i>= 0; i--){
-      output += `${newArr[i]} `;
-      
-        
-    }
-    console.log(output);
+  let newArr = [];
+ 
+
+  for (let i = n - 1; i >= 0; i--) {
+    newArr.push(arr[i]);
+  }
+  console.log(...newArr);
 }
 
-reverse(3, [10, 20, 30, 40, 50])
+reverse(3, [10, 20, 30, 40, 50]);
---
+++ 06.Arrays/demo.js
@@ -0,0 +1,10 @@
+function solve(sep){
+ 
+let arr = ["one","two"];
+
+console.log(arr.join(sep));
+
+}
+
+
+solve("-")
