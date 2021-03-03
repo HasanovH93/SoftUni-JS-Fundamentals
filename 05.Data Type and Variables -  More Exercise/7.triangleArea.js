742bd2e575a8c26dab97176891ac90edde1dd04f
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-03 10:21:55 +0200

    Data Type and Variables Exercise

--- 05.Data Type and Variables -  More Exercise/7.triangleArea.js
+++ 05.Data Type and Variables -  More Exercise/7.triangleArea.js
@@ -1,8 +1,6 @@
-function area(a,b,c){
-
-   let s = (a+b+c) / 2;
-   let area = Math.sqrt(s * (s - a) * (s - b) * (s - c))
-   console.log(area)
-
+function area(a, b, c) {
+  let s = (a + b + c) / 2;
+  let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  console.log(area);
 }
-area(2,3.5,4)
+area(2, 3.5, 4);
