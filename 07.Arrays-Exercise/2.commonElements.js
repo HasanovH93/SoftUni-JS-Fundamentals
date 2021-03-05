9b07719e5fc035e3c64044d067aa9275f9e33079
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-05 21:55:00 +0200

    Arrays

--- 07.Arrays-Exercise/2.commonElements.js
+++ 07.Arrays-Exercise/2.commonElements.js
@@ -1,18 +1,15 @@
-function solve(firstArr,secondArr){
-
-    for(let firstArrElement of firstArr){
-        for(let secArrElement of secondArr){
-
-            if(firstArrElement === secArrElement){
-                console.log(firstArrElement)
-            }
-        }
+function solve(firstArr, secondArr) {
+    
+  for (let firstArrElement of firstArr) {
+    for (let secArrElement of secondArr) {
+      if (firstArrElement === secArrElement) {
+        console.log(firstArrElement);
+      }
     }
-
+  }
 }
 
 solve(
-['Hey', 'hello', 2, 4, 'Peter', 'e'],
-['Petar', 10, 'hey', 4, 'hello', '2']
-
-)
+  ["Hey", "hello", 2, 4, "Peter", "e"],
+  ["Petar", 10, "hey", 4, "hello", "2"]
+);
