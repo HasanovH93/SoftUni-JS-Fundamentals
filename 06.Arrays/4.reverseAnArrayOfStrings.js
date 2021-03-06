843d42823bde279324c171c7737f46a77ac18578
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-06 10:21:55 +0200

    Arrays

--- 06.Arrays/4.reverseAnArrayOfStrings.js
+++ 06.Arrays/4.reverseAnArrayOfStrings.js
@@ -1,10 +1,9 @@
 function solve(arr) {
-    let revArr = [];
-    for(let i = arr.length - 1; i >= 0; i--) {
-        revArr.push(arr[i])
-
-    }
-    console.log(revArr.join(" "))
+  let revArr = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    revArr.push(arr[i]);
+  }
+  console.log(revArr.join(" "));
 }
 
-solve(['a','b','c','d','e'])
+solve(["a", "b", "c", "d", "e"]);
