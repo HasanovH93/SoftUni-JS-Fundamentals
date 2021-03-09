64e7236e1abf9bcb27b1d27b69839272dc7e0ae1
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-09 10:21:55 +0200

    Arrays

--- 06.Arrays/7.equalArrays.js
+++ 06.Arrays/7.equalArrays.js
@@ -1,21 +1,16 @@
-function solve(arr1,arr2) {
-     
-    let isIdentical = true;
-    let sum = 0;
-    for (let i = 0; i<arr1.length; i++) {
-        sum+= Number(arr1[i]);
-        if(arr1[i] !== arr2[i]) {
-            isIdentical = false
-            console.log(`Arrays are not identical. Found difference at ${i} index`);
-            break;
-
-        }
+function solve(arr1, arr2) {
+  let isIdentical = true;
+  let sum = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    sum += Number(arr1[i]);
+    if (arr1[i] !== arr2[i]) {
+      isIdentical = false;
+      console.log(`Arrays are not identical. Found difference at ${i} index`);
+      break;
     }
-    if(isIdentical) {
-        console.log(`Arrays are identical. Sum: ${sum}`);
-    }
-    
-   
-
+  }
+  if (isIdentical) {
+    console.log(`Arrays are identical. Sum: ${sum}`);
+  }
 }
-solve(['1','2'], ['1','2'])
+solve(["1", "2"], ["1", "2"]);
