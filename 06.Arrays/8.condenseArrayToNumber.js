bfbb3844db7d0a04112cdd08232aeec122d6b624
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-10 10:21:55 +0200

    Arrays

--- 06.Arrays/8.condenseArrayToNumber.js
+++ 06.Arrays/8.condenseArrayToNumber.js
@@ -1,20 +1,16 @@
 function solve(arr) {
+  while (arr.length > 1) {
+    let newArr = new Array();
 
-   while(arr.length > 1) { 
-        let newArr = new Array();
-
-        for(let i = 0 ; i < arr.length - 1; i++) {
-            let num1 = arr[i];
-            let num2 = arr[i+1];
-            let sum = num1+ num2;
-            newArr.push(sum);
-        }
-        arr = newArr;
-       
+    for (let i = 0; i < arr.length - 1; i++) {
+      let num1 = arr[i];
+      let num2 = arr[i + 1];
+      let sum = num1 + num2;
+      newArr.push(sum);
     }
-    
-    console.log(...arr)
- 
+    arr = newArr;
+  }
 
+  console.log(...arr);
 }
-solve([2,10,3]);
+solve([2, 10, 3]);
