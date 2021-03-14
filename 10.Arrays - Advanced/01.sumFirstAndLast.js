868e2f1625ba018a498e09a6e91e2d8b71c1b99b
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-14 21:55:00 +0200

    Advanced Arrays  Exercise

--- 10.Arrays - Advanced/01.sumFirstAndLast.js
+++ 10.Arrays - Advanced/01.sumFirstAndLast.js
@@ -1,14 +1,10 @@
-function sumNumbers(arr){ 
+function sumNumbers(arr) {
+  let sArray = arr.shift();
+  let fArray = arr.pop();
 
+  let result = fArray + sArray;
 
-    let sArray = arr.shift();
-    let fArray = arr.pop();
-    
-
-    let result = fArray + sArray;
-
-    console.log(Number(result))
-
+  console.log(Number(result));
 }
 
-sumNumbers([20,30,40])
+sumNumbers([20, 30, 40]);
