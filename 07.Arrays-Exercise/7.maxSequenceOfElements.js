3afe3dd41c17a587b465b82a77a78759552ccd20
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-15 20:21:55 +0200

    Arrays Exercise

--- 07.Arrays-Exercise/7.maxSequenceOfElements.js
+++ 07.Arrays-Exercise/7.maxSequenceOfElements.js
@@ -1,24 +1,20 @@
-function sequenceOfElements(arr){
-    
-    let newArr = [];
-    let secArr = [];
-    for(let i = 0;i < arr.length; i++){
-        secArr = [];
-        for(let j = i; j < arr.length; j++){
-            if(arr[i] === arr[j]){
-                secArr.push(arr[j]);
-            }else {
-                break;
-            }
-            if(secArr.length > newArr.length){
-                newArr = secArr;
-            }
-        }
-        
+function sequenceOfElements(arr) {
+  let newArr = [];
+  let secArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    secArr = [];
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        secArr.push(arr[j]);
+      } else {
+        break;
+      }
+      if (secArr.length > newArr.length) {
+        newArr = secArr;
+      }
     }
+  }
 
-console.log(newArr.join(" "));
-
-
+  console.log(newArr.join(" "));
 }
-sequenceOfElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1])
+sequenceOfElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
