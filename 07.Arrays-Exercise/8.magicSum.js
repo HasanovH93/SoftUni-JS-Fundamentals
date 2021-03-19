dbc631a10c767f7b92e33a2dfbb5511a440b16fd
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-19 21:55:00 +0200

    Arrays

---
+++ 07.Arrays-Exercise/8.magicSum.js
@@ -0,0 +1,16 @@
+function magicSum(arr,magicNum){
+
+    for(let i = 0; i < arr.length - 1; i++) {
+        let currentElement = arr[i];
+        for(let j = i+1; j < arr.length; j++) {
+            let nextElement = arr[j];
+            let sum = currentElement + nextElement;
+          if(sum === magicNum){
+              console.log(`${currentElement} ${nextElement}`)
+          }
+        }
+        
+    }
+
+}
+magicSum([1, 7, 6, 2, 19, 23],8)
