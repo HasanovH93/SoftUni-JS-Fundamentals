ceddee707b7146e10e604bb0bf753a658bd8bd13
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-21 21:55:00 +0200

    Functions Exercise

--- 09.Functions - Exercise/04.oddAndEvenSum.js
+++ 09.Functions - Exercise/04.oddAndEvenSum.js
@@ -1,25 +1,21 @@
 function printResult(input) {
+  let inputAsString = input.toString();
 
-    let inputAsString = input.toString();
-    
-    let evenSum = 0;
-    let oddSum = 0;
-   
-    let inputL = inputAsString.length
-    for(let i = 0; i < inputL; i++) {
-     let currElement = Number(inputAsString[i]);
+  let evenSum = 0;
+  let oddSum = 0;
 
-     if(currElement % 2 === 0) {
-       evenSum += currElement
-     }else {
-       oddSum += currElement
-     }
+  let inputL = inputAsString.length;
+  for (let i = 0; i < inputL; i++) {
+    let currElement = Number(inputAsString[i]);
+
+    if (currElement % 2 === 0) {
+      evenSum += currElement;
+    } else {
+      oddSum += currElement;
     }
+  }
 
-    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
-    
-
-  
+  console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
 }
 
 printResult(1000435);
