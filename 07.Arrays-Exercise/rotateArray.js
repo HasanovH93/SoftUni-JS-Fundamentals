59a899b27853bed1cbb7f7578a1e0c22373b00d4
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-22 20:21:55 +0200

    Arrays Exercise

--- 07.Arrays-Exercise/rotateArray.js
+++ 07.Arrays-Exercise/rotateArray.js
@@ -1,14 +1,10 @@
-function rotateArray(arr){
-
-    let rotations = arr.pop();
-    for(let i = 1; i <= rotations ; i++){
-     
-        let lastElement = arr.pop();
-        arr.unshift(lastElement);
-        
-    }
-  console.log(arr.join(" "))
-  
+function rotateArray(arr) {
+  let rotations = arr.pop();
+  for (let i = 1; i <= rotations; i++) {
+    let lastElement = arr.pop();
+    arr.unshift(lastElement);
+  }
+  console.log(arr.join(" "));
 }
 
-rotateArray(['1', '2', '3', '4', '2'])
+rotateArray(["1", "2", "3", "4", "2"]);
