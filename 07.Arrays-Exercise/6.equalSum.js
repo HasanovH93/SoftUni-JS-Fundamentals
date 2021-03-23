f61483d66ac562c7d01236ab185e0e9a734bc655
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-23 21:55:00 +0200

    Arrays

--- 07.Arrays-Exercise/6.equalSum.js
+++ 07.Arrays-Exercise/6.equalSum.js
@@ -1,20 +1,12 @@
-function solve(arr) { 
-    let result = 'no'; 
-    for (let i = 0; i < arr.length ; i++) { 
-        let leftSum = 0;
-        let rightSum = 0; 
-        for (let j = 0; j < i; j++) {
-            leftSum += arr[j];
-        } 
-        for (let k = arr.length - 1; k > i; k--) {
-            rightSum += arr[k];
-        } 
-        if (leftSum === rightSum){
-            result = i;
-            break;
-        }
-    } 
-    console.log(result);
+function solve(arr) {
+  let lastNum = arr.pop();
+  let resultArr = [];
+  for (let index in arr) {
+    if (index % lastNum === 0) {
+      resultArr.push(arr[index]);
+    }
+  }
+  console.log(resultArr.join(" "));
 }
 
-solve([1, 2, 3, 3])
+solve(["5", "20", "31", "4", "20", "2"]);
--- 07.Arrays-Exercise/printN-thElement.js
+++ 07.Arrays-Exercise/printN-thElement.js
@@ -1,21 +1,20 @@
-function maxNumber(arr) {
+function solve(arr) {
   let newArr = [];
   for (let i = 0; i < arr.length; i++) {
-    let currElement = arr[i];
+    let firstElement = arr[i];
     let isTopInteger = true;
-    for (let j = i + 1; j < arr.length; j++) {
+    for (j = i + 1; j < arr.length; j++) {
       let nextElement = arr[j];
-
-      if (currElement <= nextElement) {
+      if (firstElement <= nextElement) {
         isTopInteger = false;
         break;
       }
-      
     }
     if (isTopInteger) {
-        newArr.push(currElement);
-      }
+      newArr.push(firstElement);
+    }
   }
+
   console.log(newArr.join(" "));
 }
-maxNumber([1, 4, 3, 2]);
+solve([1, 4, 3, 2]);
