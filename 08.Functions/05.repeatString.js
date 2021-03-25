b48ffb90e228fe6883d6b785f99c29d75a678571
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-25 13:11:23 +0200

    Functions

--- 08.Functions/05.repeatString.js
+++ 08.Functions/05.repeatString.js
@@ -1,9 +1,8 @@
-function repeatS(string,num){
-
-    let output = '';
-    for (let i = 0; i < num; i++) {
-        output += string
-    }
-    console.log(output)
+function repeatS(string, num) {
+  let output = "";
+  for (let i = 0; i < num; i++) {
+    output += string;
+  }
+  console.log(output);
 }
-repeatS("abc",5)
+repeatS("abc", 5);
