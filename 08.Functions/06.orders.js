dd28b42f747e4ccc231e6dbd9675fff7ca890318
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-27 13:11:23 +0200

    Functions

--- 08.Functions/06.orders.js
+++ 08.Functions/06.orders.js
@@ -1,20 +1,19 @@
 function ordersP(product, quantity) {
-
-    let totalPrice = 0;
+  let totalPrice = 0;
   switch (product) {
     case "coffee":
-        totalPrice = quantity * 1.50
+      totalPrice = quantity * 1.5;
       break;
     case "water":
-        totalPrice = quantity * 1
+      totalPrice = quantity * 1;
       break;
     case "coke":
-        totalPrice = quantity * 1.40
+      totalPrice = quantity * 1.4;
       break;
     case "snacks":
-        totalPrice = quantity * 2
+      totalPrice = quantity * 2;
       break;
   }
-  console.log(totalPrice.toFixed(2))
+  console.log(totalPrice.toFixed(2));
 }
-ordersP("water",5)
+ordersP("water", 5);
