f6ff7cf0e432cae1ecf32f0b6f9809b3c1fc5cb0
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-28 21:55:00 +0200

    Functions Exercise

--- 09.Functions - Exercise/03.charactersInRange.js
+++ 09.Functions - Exercise/03.charactersInRange.js
@@ -1,9 +1,11 @@
 function charRange(fChar, sChar) {
   
   let firstSymbolAsNumber = fChar.charCodeAt(0);
-  let secondSumbolAsNumber = sChar.charCodeAt(0);;
+  let secondSumbolAsNumber = sChar.charCodeAt(0);
+  let smallerNumber = Math.min(firstSymbolAsNumber, secondSumbolAsNumber);
+  let biggerNumbe = Math.max(firstSymbolAsNumber,secondSumbolAsNumber)
   let arr = [];
-  for(let i = firstSymbolAsNumber + 1; i < secondSumbolAsNumber; i++){
+  for(let i = smallerNumber + 1; i < biggerNumbe; i++) {
     let currElement = String.fromCharCode(i);
     arr.push(currElement)
   }
