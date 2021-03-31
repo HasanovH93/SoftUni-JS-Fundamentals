f595b04447203db0da1b2b4d69cf916aba684bd9
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-31 21:55:00 +0200

    Functions Exercise

--- 09.Functions - Exercise/03.charactersInRange.js
+++ 09.Functions - Exercise/03.charactersInRange.js
@@ -1,18 +1,14 @@
 function charRange(fChar, sChar) {
+  
   let firstSymbolAsNumber = fChar.charCodeAt(0);
-  let secondSymbolAsNumber = sChar.charCodeAt(0);
-  let smallerNumber = Math.min(firstSymbolAsNumber,secondSymbolAsNumber);
-  let biggerNumber = Math.max(firstSymbolAsNumber,secondSymbolAsNumber);
-
-
-  let result = [];
-
-  for (let i = smallerNumber + 1; i < biggerNumber; i++) {
-    let currentElement = String.fromCharCode(i);
-    result.push(currentElement);
-    
+  let secondSumbolAsNumber = sChar.charCodeAt(0);;
+  let arr = [];
+  for(let i = firstSymbolAsNumber + 1; i < secondSumbolAsNumber; i++){
+    let currElement = String.fromCharCode(i);
+    arr.push(currElement)
   }
-  console.log(result.join(" "));
+  
+   console.log(arr.join(" "))
 }
 
 charRange("a", "d");
