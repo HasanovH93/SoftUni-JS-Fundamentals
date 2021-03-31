9d365203510236acc8f9c1124f189a28d5e47e9f
Hasan Hasanov <hasanovh14@gmail.com>
2021-03-31 21:55:00 +0200

    Functions

--- 09.Functions - Exercise/02.1.addAndSubtract.js
+++ 09.Functions - Exercise/02.1.addAndSubtract.js
@@ -1,5 +1,20 @@
 function printResult(fNum, sNum, tNum) {
-    console.log(fNum+sNum-tNum)
+    
+    // console.log(fNum + sNum - tNum);  // One Line Solution
+    
+    function sumOfTwoNumbers(fNum, sNum){
+      return fNum + sNum;
+    }
+    function subtract(sum,tNum){
+        return sum - tNum;
+    }
+
+    let sum = sumOfTwoNumbers(fNum, sNum);
+   
+    let finalResult = subtract(sum,tNum);
+
+    console.log(finalResult);
 }
+
 
 printResult(23,6,10)
