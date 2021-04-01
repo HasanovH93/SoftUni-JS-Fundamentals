c9bb2f80063127064557931386ac1b4d409a7df6
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-01 13:11:23 +0200

    Functions Exercise

--- 09.Functions - Exercise/05.palindromeIntegers.js
+++ 09.Functions - Exercise/05.palindromeIntegers.js
@@ -1,25 +1,20 @@
-function palindromeInt (input) {
-    
-    let arrOfNumber = input;
-    let inputL = arrOfNumber.length;
-    const isPalindrome = (num) => {    
-    
-        let startNum = num;
-        let reverseNum = Number(num.toString().split("").reverse().join(""));
-        
-        if(startNum === reverseNum){
-            return true;
-        }else {
-            return false;
-        }
-    }
-    for(let i = 0; i < inputL; i++) {
-        let currNumber = arrOfNumber[i];
-        console.log(isPalindrome(currNumber));
-       
+function palindromeInt(input) {
+  let arrOfNumber = input;
+  let inputL = arrOfNumber.length;
+  const isPalindrome = (num) => {
+    let startNum = num;
+    let reverseNum = Number(num.toString().split("").reverse().join(""));
 
+    if (startNum === reverseNum) {
+      return true;
+    } else {
+      return false;
     }
-
+  };
+  for (let i = 0; i < inputL; i++) {
+    let currNumber = arrOfNumber[i];
+    console.log(isPalindrome(currNumber));
+  }
 }
 
-palindromeInt([123,323,421,121]);
+palindromeInt([123, 323, 421, 121]);
