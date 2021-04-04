d823a69ab2ff85f43299ede54f561bf8f7effeb6
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-04 13:11:23 +0200

    Arrays Advanced

--- 10.Arrays - Advanced/02.negativePositiveNumbers.js
+++ 10.Arrays - Advanced/02.negativePositiveNumbers.js
@@ -1,18 +1,16 @@
-function numbersSorting(input){
-     
-    let newArr = []
-    for(let i = 0; i < input.length; i++){
-        let element = Number(input[i]);
-        if(element < 0){
-          newArr.unshift(element);
-        }else {
-            newArr.push(element)
-        }
+function numbersSorting(input) {
+  let newArr = [];
+  for (let i = 0; i < input.length; i++) {
+    let element = Number(input[i]);
+    if (element < 0) {
+      newArr.unshift(element);
+    } else {
+      newArr.push(element);
     }
-    for(const element of newArr){
-        console.log(element)
-    }
-    
+  }
+  for (const element of newArr) {
+    console.log(element);
+  }
 }
 
-numbersSorting(['3', '-2', '0', '-1'])
+numbersSorting(["3", "-2", "0", "-1"]);
