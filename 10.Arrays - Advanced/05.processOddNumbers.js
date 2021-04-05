babcd904f326a720f39141ca7527f7f130ba32cd
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-05 13:11:23 +0200

    Arrays Advanced

--- 10.Arrays - Advanced/05.processOddNumbers.js
+++ 10.Arrays - Advanced/05.processOddNumbers.js
@@ -1,11 +1,9 @@
-function oddNumbers(arr){
-
+function oddNumbers(arr) {
   let oddIndexes = arr.filter((x, i) => i % 2 === 1);
 
-  let doubled = oddIndexes.map(x => x * 2);
-  
-  let result = doubled.reverse().join(' ');
-  console.log(result);
+  let doubled = oddIndexes.map((x) => x * 2);
 
+  let result = doubled.reverse().join(" ");
+  console.log(result);
 }
 oddNumbers([10, 15, 20, 25]);
