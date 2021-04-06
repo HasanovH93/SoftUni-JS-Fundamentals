123e7126e0cdaa8f7a7cc5bf5766d6c0a963ff07
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-06 13:11:23 +0200

    Arrays Advanced

--- 10.Arrays - Advanced/06.smallestTwoNumbers.js
+++ 10.Arrays - Advanced/06.smallestTwoNumbers.js
@@ -1,7 +1,8 @@
-function smallestNumbers(arr){
-
-    let sorted = arr.sort((a,b) => a-b).slice(0,2).join(' ');
-    console.log(sorted);
-
+function smallestNumbers(arr) {
+  let sorted = arr
+    .sort((a, b) => a - b)
+    .slice(0, 2)
+    .join(" ");
+  console.log(sorted);
 }
 smallestNumbers([30, 15, 50, 5]);
