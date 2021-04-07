d2354dc6dbc5dd82c825ab15cb8027ec00f5b9c3
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-07 13:25:55 +0200

    Advanced Arrays Exercise

--- 11.Arrays - Advanced - Exercise/02.distrinctAray.js
+++ 11.Arrays - Advanced - Exercise/02.distrinctAray.js
@@ -1,11 +1,5 @@
 function district(arr) {
-  let resultArr = [];
-  for (const element of arr) {
-    if (!resultArr.includes(element)) {
-      resultArr.push(element);
-    }
-  }
-  console.log(resultArr.join(' '));
+
 }
 
 district([7, 8, 9, 7, 2, 3, 4, 1, 2]);
