50d0bc8eefe8235e3532f51216d9cebcc86586f2
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-07 13:25:55 +0200

    Advanced Arrays Exercise

---
+++ 11.Arrays - Advanced - Exercise/train.js
@@ -0,0 +1,29 @@
+function train(arr){
+    
+    let passangersInWagons = arr.shift().split(' ').map(Number);
+    let maxCapacity = Number(arr.shift());
+    
+    for (const command of arr ) {
+        let currentCommand = command.split(' ');
+        if(currentCommand[0] === 'Add'){
+              passangersInWagons.push(Number(currentCommand[1]));  
+        }else{
+            for(let i = 0; i < passangersInWagons.length; i++){
+                if(passangersInWagons[i] + Number(currentCommand[0]) <= maxCapacity ){
+                   passangersInWagons[i] += Number(currentCommand[0]);
+                   break;
+                }
+            }
+        }
+    }
+
+console.log(passangersInWagons.join(' '))
+}
+train(['32 54 21 12 4 0 23',
+'75',
+'Add 10',
+'Add 0',
+'30',
+'10',
+'75']
+)
