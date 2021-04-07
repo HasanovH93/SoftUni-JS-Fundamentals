69c7f29d819d6fb2e954078bad1588f873953124
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-07 13:11:23 +0200

    Arrays Advanced

--- 11.Arrays - Advanced - Exercise/05.sortBy2Criteria.js
+++ 11.Arrays - Advanced - Exercise/05.sortBy2Criteria.js
@@ -1,12 +1,7 @@
-function sorted(arr){
-     
-    let sorted = (a,b) => 
-    a.length-b.length || a.localeCompare(b);
-    arr.sort(sorted);
-    
-    console.log(arr.join("\n"))
+function sorted(arr) {
+  let sorted = (a, b) => a.length - b.length || a.localeCompare(b);
+  arr.sort(sorted);
+
+  console.log(arr.join("\n"));
 }
-sorted(['Deny',
-    'omen',
-    'test',
-    'Default'])
+sorted(["Deny", "omen", "test", "Default"]);
