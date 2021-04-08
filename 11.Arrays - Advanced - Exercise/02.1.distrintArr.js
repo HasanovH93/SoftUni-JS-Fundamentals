29b8a2b7ef9937ec28a0d87e857d9b96e66413e4
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-08 13:25:55 +0200

    Advanced Arrays Exercise

---
+++ 11.Arrays - Advanced - Exercise/02.1.distrintArr.js
@@ -0,0 +1,16 @@
+function distrArr(arr){
+
+    for(let i = 0; i < arr.length; i++){
+        let currElement = arr[i];
+        for(let j = i + 1; j < arr.length; j++){
+            let nextElement = arr[j];
+            if(currElement === nextElement){
+                
+                arr.splice(j, 1);
+                i--;
+            }
+        }
+    }
+   console.log(arr.join(' '))
+}
+distrArr([7, 8, 9, 7, 2, 3, 4, 1,1, 2])
