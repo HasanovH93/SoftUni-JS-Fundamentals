02e5d05741d61613d8303d5e94b2cd5a402b42e2
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-10 13:11:23 +0200

    Mid Exam Preparation

--- 12.Mid Exam Preparation/01.bonusScoringSystem.js
+++ 12.Mid Exam Preparation/01.bonusScoringSystem.js
@@ -7,14 +7,13 @@ function solve(input) {
   let lecturesCount = input.shift();
   let maxBonus = (lecturesCount / lectures) * (5 + bonus);
   maxBonus = Math.ceil(maxBonus);
-  if(input.length > 0){
+  if (input.length > 0) {
     console.log(`Max Bonus: ${maxBonus}.`);
     console.log(`The student has attended ${lecturesCount} lectures.`);
-  }else {
+  } else {
     console.log(`Max Bonus: 0.`);
     console.log(`The student has attended 0 lectures.`);
   }
- 
 }
 
 solve([
