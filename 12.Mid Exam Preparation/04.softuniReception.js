05c336ba73e9c3172e0cb22cd4863c86791c6e48
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-13 13:25:55 +0200

    Advanced Arrays Exercise

--- 12.Mid Exam Preparation/04.softuniReception.js
+++ 12.Mid Exam Preparation/04.softuniReception.js
@@ -1,24 +1,24 @@
-function reception(arr){
-    
-   let employee1 = Number(arr[0]);
-   let employee2 = Number(arr[1]);
-   let employee3 = Number(arr[2]);
-   let students = Number(arr[3]);
-   
-   let employeeEficiency = employee1 + employee2 + employee3;
-    let hours = 0;
+function reception(arr) {
+  let students = arr.pop();
+  let arrToNum = arr.map(Number);
+  let employeeEficiency = arrToNum.reduce((a,b)=>a+b,0);
+//   let employee1 = Number(arr[0]);
+//   let employee2 = Number(arr[1]);
+//   let employee3 = Number(arr[2]);
+//   let students = Number(arr[3]);
 
-    while(students > 0){
-        hours++
-        if(hours % 4 == 0){
-            continue;
-        }else {
-          students -= employeeEficiency
-        }
-       
+//   let employeeEficiency = employee1 + employee2 + employee3;
+  let hours = 0;
+
+  while (students > 0) {
+    hours++;
+    if (hours % 4 == 0) {
+      continue;
+    } else {
+      students -= employeeEficiency;
     }
+  }
 
-    console.log(`Time needed: ${hours}h.`)
-
+  console.log(`Time needed: ${hours}h.`);
 }
-reception(['1','2','3','45'])
+reception(["1", "2", "3", "45"]);
