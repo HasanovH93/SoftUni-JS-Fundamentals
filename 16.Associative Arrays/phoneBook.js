ef4343cac82f3a603e026d8ed8678f622d417588
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-13 20:21:55 +0200

    Associative Arrays

--- 16.Associative Arrays/phoneBook.js
+++ 16.Associative Arrays/phoneBook.js
@@ -1,3 +1,18 @@
-function phoneBook(arr){
-    
+function phoneBook(arr) {
+    let result = {};
+    for (const line of arr) {
+       let tokens = line.split(' ');
+       let name = tokens[0];
+       let phone = tokens[1];
+       result[name] = phone;
+    }
+    for (const name in result) {
+        console.log(`${name} -> ${result[name]}`);
+    }
 }
+phoneBook([
+  "Tim 0834212554",
+  "Peter 0877547887",
+  "Bill 0896543112",
+  "Tim 0876566344",
+]);
