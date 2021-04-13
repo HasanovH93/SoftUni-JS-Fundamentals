3e86b62ba44ebbc2ad544d481d1569d4b33cb55b
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-13 20:21:55 +0200

    Associative Arrays

---
+++ 16.Associative Arrays/02.meetings.js
@@ -0,0 +1,17 @@
+function meetings(array) {
+  let result = {};
+  for (const line of array) {
+    let [day, name] = line.split(" ");
+    if (result.hasOwnProperty(day)) {
+        console.log(`Conflict on ${day}!`)
+    } else {
+      result[day] = name;
+      console.log(`Scheduled for ${day}`)
+    }
+  }
+
+  for (const day in result) {
+    console.log(day, "->", result[day]);
+  }
+}
+meetings(["Monday Peter", "Wednesday Bill", "Monday Tim", "Friday Tim"]);
