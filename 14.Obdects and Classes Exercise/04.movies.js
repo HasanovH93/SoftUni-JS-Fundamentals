4100a12bb31efebdfcfa71b906878946a4a26034
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-14 20:21:55 +0200

    Object and Classes Exercise

--- 14.Obdects and Classes Exercise/04.movies.js
+++ 14.Obdects and Classes Exercise/04.movies.js
@@ -1,5 +1,36 @@
 function moviesSTore(input) {
-
+    let listOfMovies = []
+  for (const lines of input) {
+    if(lines.includes("addMovie")){
+      let nameOfMovie = lines.split('addMovie ')[1];
+      listOfMovies.push({ name: nameOfMovie }) ;
+    }else if(lines.includes('directedBy')){
+      let info = lines.split('directedBy')
+      let name = info[0].trim();
+      let director = info[1].trim();
+      let movie = listOfMovies.find((movieObj) => movieObj.name === name);
+      if(movie){
+        movie.director = director;
+      }
+    }else if(lines.includes("onDate")){
+      let info = lines.split("onDate ");
+      let name = info[0].trim();
+      let date = info[1];
+      let movie = listOfMovies.find((movieObj) => movieObj.name === name);
+      if(movie) {
+        movie.date = date
+      }
+    }
+    
+  }
+  for (const movie of listOfMovies) {
+    if(movie.name && movie.director && movie.date) {
+      let movieToJSON = JSON.stringify(movie);
+      console.log(movieToJSON);
+      
+    }
+   
+  }
 
   
 }
