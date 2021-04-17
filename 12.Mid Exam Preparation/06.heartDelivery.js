33bcbd18f20b179acbe692f45d574e54f7ac83aa
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-17 21:55:00 +0200

    Exam Preparation

--- 12.Mid Exam Preparation/06.heartDelivery.js
+++ 12.Mid Exam Preparation/06.heartDelivery.js
@@ -1,25 +1,25 @@
 function heartDelivery(arr) {
   let houses = arr.shift().split("@").map(Number);
   let cupid = 0;
-  while (arr[0] !== "Love!") {
+  while (arr[0] != "Love!") {
     let tokens = arr.shift().split(" ");
-    let jump = +tokens[1];
+    let jump = Number(tokens[1]);
     cupid += jump;
     if (cupid >= houses.length) {
       cupid = 0;
     }
-    if (houses[cupid] === 0) {
-      console.log(`${cupid} already had Valentine's day.`);
+    if (houses[cupid] == 0) {
+      console.log(`Place ${cupid} already had Valentine's day.`);
     } else {
       houses[cupid] -= 2;
-      if (houses[cupid] === 0) {
+      if (houses[cupid] == 0) {
         console.log(`Place ${cupid} has Valentine's day.`);
       }
     }
   }
 
   let missed = 0;
-  for (const house of houses) {
+  for (let house of houses) {
     if (house > 0) {
       missed++;
     }
@@ -28,7 +28,7 @@ function heartDelivery(arr) {
   if (missed > 0) {
     console.log(`Cupid has failed ${missed} places.`);
   } else {
-      console.log(`Mission was successful`)
+    console.log(`Mission was successful.`);
   }
 }
 heartDelivery(["10@10@10@2", "Jump 1", "Jump 2", "Love!"]);
