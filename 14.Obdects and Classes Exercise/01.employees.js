d964d96ae42470673cdcbdfbda63813d42f3af9b
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-19 20:21:55 +0200

    Object and Classes Exercise

--- 14.Obdects and Classes Exercise/01.employees.js
+++ 14.Obdects and Classes Exercise/01.employees.js
@@ -1,22 +1,17 @@
 function employeesPersonalNumber(input) {
-
-    for (const name of input) {
-        let currName = name;
-        let obj = {
-            name: currName,
-            personalNum: currName.length
-        };
-        console.log(`Name: ${currName} -- Personal Number:${obj.personalNum}`);
-        
-    }
-  
-
+  for (const name of input) {
+    let currName = name;
+    let obj = {
+      name: currName,
+      personalNum: currName.length,
+    };
+    console.log(`Name: ${obj.name} -- Personal Number: ${obj.personalNum}`);
+  }
 }
 
-
 employeesPersonalNumber([
-    'Silas Butler',
-    'Adnaan Buckley',
-    'Juan Peterson',
-    'Brendan Villarreal'
-    ])
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
