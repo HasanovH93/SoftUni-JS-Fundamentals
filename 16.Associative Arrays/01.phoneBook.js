8331007a9f36540f84dacb14cdf7646d95796b1b
Hasan Hasanov <hasanovh14@gmail.com>
2021-04-21 20:21:55 +0200

    Associative Arrays

--- 16.Associative Arrays/01.phoneBook.js
+++ 16.Associative Arrays/01.phoneBook.js
@@ -1,13 +1,24 @@
 function phoneBook(arr) {
-    let result = {};
+    // let result = {};
+    // for (const line of arr) {
+    //    let tokens = line.split(' ');
+    //    let name = tokens[0];
+    //    let phone = tokens[1];
+    //    result[name] = phone;
+    // }
+    // for (const name in result) {
+    //     console.log(`${name} -> ${result[name]}`);
+    // }
+
+    let result = new Map();
     for (const line of arr) {
-       let tokens = line.split(' ');
-       let name = tokens[0];
-       let phone = tokens[1];
-       result[name] = phone;
+      let [name, phone] = line.split(' ');
+
+      result.set(name,phone);
+      
     }
-    for (const name in result) {
-        console.log(`${name} -> ${result[name]}`);
+    for(let key of result.keys()) {
+      console.log(key, '->', result.get(key));
     }
 }
 phoneBook([
---
+++ 16.Associative Arrays/04.storage.js
@@ -0,0 +1,18 @@
+function storage(arr) {
+  let result = new Map();
+  for (const line of arr) {
+    let [product, quantity] = line.split(" ");
+    quantity = Number(quantity);
+
+    if (result.has(product)) {
+      let existing = result.get(product);
+      result.set(product, quantity + existing);
+    } else {
+      result.set(product, quantity);
+    }
+  }
+  for (const [product, quantity] of result) {
+    console.log(product, "->", quantity);
+  }
+}
+storage(["tomatoes 10", "coffee 5", "olives 100", "coffee 40"]);
