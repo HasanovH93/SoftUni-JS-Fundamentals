1c234f2e09fc38201faadb9af3f809640312ad10
Hasan Hasanov <hasanovh14@gmail.com>
2022-04-30 21:04:04 +0200

    Advanced Arrays Exercise

--- 11.Arrays - Advanced - Exercise/01.train.js
+++ 11.Arrays - Advanced - Exercise/01.train.js
@@ -1,29 +1,21 @@
-function train(arr){
-    
-    let passangersInWagons = arr.shift().split(' ').map(Number);
-    let maxCapacity = Number(arr.shift());
-    
-    for (const command of arr ) {
-        let currentCommand = command.split(' ');
-        if(currentCommand[0] === 'Add'){
-              passangersInWagons.push(Number(currentCommand[1]));  
-        }else{
-            for(let i = 0; i < passangersInWagons.length; i++){
-                if(passangersInWagons[i] + Number(currentCommand[0]) <= maxCapacity ){
-                   passangersInWagons[i] += Number(currentCommand[0]);
-                   break;
-                }
-            }
-        }
-    }
+function train(arr) {
+  let passangersInWagons = arr.shift().split(" ").map(Number);
+  let maxCapacity = Number(arr.shift());
 
-console.log(passangersInWagons.join(' '))
+  for (const command of arr) {
+    let currentCommand = command.split(" ");
+    if (currentCommand[0] === "Add") {
+      passangersInWagons.push(Number(currentCommand[1]));
+    } else {
+      for (let i = 0; i < passangersInWagons.length; i++) {
+        if (passangersInWagons[i] + Number(currentCommand[0]) <= maxCapacity) {
+          passangersInWagons[i] += Number(currentCommand[0]);
+          break;
+        }
+      }
+    }
+  }
+
+  console.log(passangersInWagons.join(" "));
 }
-train(['32 54 21 12 4 0 23',
-'75',
-'Add 10',
-'Add 0',
-'30',
-'10',
-'75']
-)
+train(["32 54 21 12 4 0 23", "75", "Add 10", "Add 0", "30", "10", "75"]);
--- 11.Arrays - Advanced - Exercise/02.1.distrintArr.js
+++ 11.Arrays - Advanced - Exercise/02.1.distrintArr.js
@@ -1,16 +1,14 @@
-function distrArr(arr){
-
-    for(let i = 0; i < arr.length; i++){
-        let currElement = arr[i];
-        for(let j = i + 1; j < arr.length; j++){
-            let nextElement = arr[j];
-            if(currElement === nextElement){
-                
-                arr.splice(j, 1);
-                i--;
-            }
-        }
+function distrArr(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let currElement = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      let nextElement = arr[j];
+      if (currElement === nextElement) {
+        i--;
+        arr.splice(j, 1);
+      }
     }
-   console.log(arr.join(' '))
+  }
+  console.log(arr.join(" "));
 }
-distrArr([7, 8, 9, 7, 2, 3, 4, 1,1, 2])
+distrArr([7, 8, 9, 7, 2, 3, 4, 1, 1, 2]);
