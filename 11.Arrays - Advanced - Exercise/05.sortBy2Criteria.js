03cc6f86ac3c80a36c7cbb875c7b2ece6efab196
Hasan Hasanov <hasanovh14@gmail.com>
2022-05-01 10:08:05 +0200

    Advanced Arrays Exercise

--- 11.Arrays - Advanced - Exercise/05.sortBy2Criteria.js
+++ 11.Arrays - Advanced - Exercise/05.sortBy2Criteria.js
@@ -1,12 +1,12 @@
-function sort(arr){
+function sorted(arr){
      
-    let sortedBy1 = arr.sort((a,b)=>a.length-b.length);
-    let sortedBy2 = sortedBy1.sort((a,b)=> b.localeCompare(a))
-    for (const element of sortedBy2) {
-        console.log(element)
-    }
+    let sorted = (a,b) => 
+    a.length-b.length || a.localeCompare(b);
+    arr.sort(sorted);
+    
+    console.log(arr.join("\n"))
 }
-sort(['Deny',
+sorted(['Deny',
     'omen',
     'test',
     'Default'])
