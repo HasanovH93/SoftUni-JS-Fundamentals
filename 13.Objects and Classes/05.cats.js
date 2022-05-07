4e13bb6c24e29c11d7d674fd7e4b384bb3282f73
Hasan Hasanov <hasanovh14@gmail.com>
2022-05-07 21:56:52 +0200

    Objects and Classess

--- 13.Objects and Classes/05.cats.js
+++ 13.Objects and Classes/05.cats.js
@@ -1,5 +1,5 @@
 function catCreator(catsArr) {
-  class Cat{
+  class Cat {
     constructor(name, age) {
       this.name = name;
       this.age = age;
@@ -10,18 +10,16 @@ function catCreator(catsArr) {
   }
   let catsInfo = [];
 
-  
   for (const cat of catsArr) {
-    let tokens = cat.split(' ');
+    let tokens = cat.split(" ");
     let cats = new Cat(tokens[0], Number(tokens[1]));
-    catsInfo.push(cats)
+    catsInfo.push(cats);
   }
 
   for (const cat of catsInfo) {
-     cat.meow()
+    cat.meow();
   }
- 
 }
 
 catCreator(["Mellow 2", "Tom 5"]);
-catCreator(['Cany 1', 'Hasan 3'])
+catCreator(["Cany 1", "Hasan 3"]);
