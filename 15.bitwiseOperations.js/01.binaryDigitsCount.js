373a399f44ddcbf14d998d6c5f4daf42cf129ebc
Hasan Hasanov <hasanovh14@gmail.com>
2022-05-09 11:09:21 +0200

    Bitwise Operations

--- 15.bitwiseOperations.js/01.binaryDigitsCount.js
+++ 15.bitwiseOperations.js/01.binaryDigitsCount.js
@@ -1,18 +1,18 @@
 function binaryCount(num, binaryNumber) {
-  let arr = [];
+//   let arr = [];
   let counter = 0;
   while (num !== 0) {
     let leftOver = num % 2;
 
     if (leftOver == binaryNumber) {
-        arr.push(0)
+        // arr.push(0)
       counter++;
     }else {
-        arr.push(1)
+        // arr.push(1)
     }
     num = parseInt(num / 2);
   }
-  console.log(arr.reverse())
+//   console.log(arr.reverse())
   console.log(counter);
 }
 
